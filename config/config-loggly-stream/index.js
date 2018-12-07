90101c06859d86b7af3a5f85ad5900c79a8bebe9
Tadd Giles <tadd@kuali.co>
2018-12-07 15:26:01 -0700

    cleanup, update dependencies, update to node 10

--- config/config-loggly-stream/index.js
+++ config/config-loggly-stream/index.js
@@ -1,11 +1,3 @@
-/* Copyright © 2018 Kuali, Inc. - All Rights Reserved
- * You may use and modify this code under the terms of the Kuali, Inc.
- * Pre-Release License Agreement. You may not distribute it.
- *
- * You should have received a copy of the Kuali, Inc. Pre-Release License
- * Agreement with this file. If not, please write to license@kuali.co.
- */
-
 // Creates direct to loggly stream for kuali-logger
 if (!process.env.LOGGLY_AUTH) {
   console.log(
